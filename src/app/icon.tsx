18d0442a05ecf01039d5bcb3a1ec655969d8d4c6
Nanphipat Wongted <nanphipat.wongted@icloud.com>
2026-07-21 19:05:10 +0700

    favicon

---
+++ src/app/icon.tsx
@@ -0,0 +1,28 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#5F5143",
+          color: "#F6F4F0",
+          fontSize: 20,
+          fontWeight: 700,
+          letterSpacing: "-1.8px",
+        }}
+      >
+        theoo
+      </div>
+    ),
+    { ...size },
+  );
+}
